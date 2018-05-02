3e8abca21d41bae7de571b1a37767c903be29ca1
Yaroslav Halivets <yarikgalivetc@gmail.com>
2018-05-02 21:07:06 +0300

    add Controllers with posts and comments

---
+++ winterCourse2018Networking/winterCourse2018Networking/UI/CommentsTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  CommentsTableViewCell.swift
+//  winterCourse2018Networking
+//
+//  Created by Yaroslav Halivets on 5/2/18.
+//  Copyright © 2018 bws2007. All rights reserved.
+//
+
+import UIKit
+
+class CommentsTableViewCell: UITableViewCell {
+    var comments : Comment?{
+        didSet{
+              guard let currentComments = comments else {return}
+            self.nameCommentLabel.text = currentComments.name
+            self.bodyCommentLabel.text = currentComments.body
+            self.emailLabel.text = currentComments.email
+        }
+    }
+    @IBOutlet weak var nameCommentLabel: UILabel!
+    
+    @IBOutlet weak var bodyCommentLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ winterCourse2018Networking/winterCourse2018Networking/UI/PostsTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostsTableViewCell.swift
+//  winterCourse2018Networking
+//
+//  Created by Yaroslav Halivets on 5/2/18.
+//  Copyright © 2018 bws2007. All rights reserved.
+//
+
+import UIKit
+
+class PostsTableViewCell: UITableViewCell {
+    var posts : Post? {
+        didSet {
+             guard let currentPost = posts else {return}
+            self.bodyLabel.text = currentPost.body
+            self.titleLabel.text = currentPost.title
+        }
+    }
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
